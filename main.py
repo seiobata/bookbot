2c5485de8ce9b8e5f237dbde5d11ce98d4fb4397
seiobata <s.bautista18@yahoo.com>
2025-01-21 17:21:25 -0600

    main includes character count function

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 def main():
     print(file_contents)
     word_count()
+    character_count()
 
 def word_count():
     words = file_contents.split()
-    count = 0
-    for word in words:
-        count += 1
+    count = len(words)
     print(f"{count} number of words in document")
+
+def character_count():
+    dict_of_chars = {}
+    lower_file_contents = file_contents.lower()
+    for i in lower_file_contents:
+        if i in dict_of_chars:
+            dict_of_chars[i] += 1
+        else:
+            dict_of_chars[i] = 1
+    print(dict_of_chars)
     
 with open("books/frankenstein.txt") as f:
     file_contents = f.read()
